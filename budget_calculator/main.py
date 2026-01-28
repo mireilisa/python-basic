6a08e0ac5c5fac4e2e7661162624765c89957673
mireilisa <163601849+mireilisa@users.noreply.github.com>
2026-01-28 21:57:04 +0200

    Add budget calculator project
    Initial version of monthly budget calculator in Python.

---
+++ budget_calculator/main.py
@@ -0,0 +1,72 @@
+def get_income():
+    pass
+
+def get_expenses(category):
+    pass
+
+def collect_expenses(categories):
+    pass
+
+def calculate_total(expenses):
+    pass
+
+def print_report(income, expenses, total, balance):
+    pass
+
+def main():
+    pass
+
+def get_income():
+    income = float(input("Enter your monthly income: "))
+    return income
+
+def get_expenses(category):
+    amount =float(input(f"Enter your monthly expenses for {category}:"))
+    return amount
+
+def collect_expenses(categories):
+    expenses = []
+    for category in categories:
+        amount = get_expenses(category)
+        expenses.append((category, amount))
+    return expenses
+
+def calculate_total(expenses):
+    total = sum(amount for category, amount in expenses)
+    return total
+
+def main():
+    income = get_income()
+
+    categories = ["Rent", "Food", "Transport", "Internet"]
+    categories.append("Pets")
+
+    expenses = collect_expenses(categories)
+    
+    total = calculate_total(expenses)
+    balance = income - total
+
+    print_report(income, expenses, total, balance)
+
+def print_report(income, expenses, total, balance):
+    print("\n--- Monthly Budget Report ---")
+    print(f"Total Income: {income:.2f}")
+    
+    print("Expenses:")
+    for category, amount in expenses:
+        print(f"- {category}: {amount:.2f}")
+
+    print(f"\nTotal Expenses: {total:.2f}")
+    print(f"Balance: {balance:.2f}")
+
+    if balance < 0:
+        print("\033[91m❌ WARNING: You are over budget!\033[0m")
+    
+    elif balance < 500: 
+        print("\033[93m⚠️ Attention: Budget is tight!\033[0m")
+
+    else:
+        print("\033[92m✅ Budget under control!\033[0m")
+
+if __name__ == "__main__":
+        main()
